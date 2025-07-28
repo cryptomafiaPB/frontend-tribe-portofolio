5fc02f55c52dc466a3015a9f5f41f25ce5c6703d
cryptomafiaPB <jugad360@gmail.com>
2025-07-28 14:01:47 +0530

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+import { Analytics } from "@vercel/analytics/react";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "Pranav Bagal",
+  description: "Pranav Bagal a Full Stack Developer and Open Source Enthusiast",
+  icons: {
+    icon: [
+      {
+        url: "/pranav-icon.ico",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <link rel="icon" href="/pranav-icon.ico" />
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
+      >
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
---
+++ src/sections/About.tsx
@@ -0,0 +1,209 @@
+"use client";
+
+import Card from "@/components/Card";
+import SectionHeader from "@/components/SectionHeader";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import nextIcon from "@/assets/icons/nextjs-icon-svgrepo-com.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import mapImage from "@/assets/images/jalgaon_map.png";
+import Resume from "@/assets/images/resume.png";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+import DockerIcon from "@/assets/icons/docker.svg";
+import CardHeader from "@/components/CardHeader";
+import ToolboxItems from "@/components/ToolboxItems";
+import { motion } from "framer-motion";
+import { useRef } from "react";
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    iconType: JavascriptIcon,
+  },
+  {
+    title: "Docker",
+    iconType: DockerIcon,
+  },
+  {
+    title: "HTML5",
+    iconType: HTMLIcon,
+  },
+  {
+    title: "CSS",
+    iconType: CssIcon,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Chrome",
+    iconType: ChromeIcon,
+  },
+  {
+    title: "Github",
+    iconType: GithubIcon,
+  },
+  {
+    title: "NextJs",
+    iconType: nextIcon,
+  },
+];
+
+const hobbies = [
+  {
+    title: "Music Production",
+    emoji: "🎶",
+    left: "5%",
+    top: "5%",
+  },
+  {
+    title: "Geopolitics",
+    emoji: "🌎",
+    left: "50%",
+    top: "5%",
+  },
+
+  {
+    title: "Hiking",
+    emoji: "🥾",
+    left: "35%",
+    top: "40%",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+    left: "10%",
+    top: "35%",
+  },
+  {
+    title: "Sports",
+    emoji: "⚽",
+    left: "70%",
+    top: "45%",
+  },
+  {
+    title: "Fitness",
+    emoji: "🏋️‍♀️",
+    left: "5%",
+    top: "65%",
+  },
+];
+
+import { saveAs } from "file-saver";
+import { title } from "process";
+
+export const AboutSection = () => {
+  const handleDownload = () => {
+    // Assuming resume.pdf is in public/assets/docs/
+
+    // Trigger download
+    const pdfUrl = "/docs/pranav_bagal.pdf";
+    fetch(pdfUrl)
+      .then((response) => {
+        console.log(response);
+        return response.blob();
+      })
+      .then((blob) => {
+        console.log(blob)
+        saveAs(blob, "pranav_bagal.pdf");
+      }).catch(
+        (error) => {
+          console.error("Error downloading PDF:", error);
+        }
+      )
+  };
+  const constraintRef = useRef(null);
+  return (
+    <div className="py-20 lg:py-28" id="about">
+      <div className="container">
+        <SectionHeader
+          title="About Me"
+          eyebrow="Am Pranav Bagal"
+          description="Full stack web developer."
+        />
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-8 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader title="get My Resume" description="Resume" className="!pb-3" />
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image
+                  src={Resume}
+                  alt="Book Cover"
+                  onClick={handleDownload}
+                  className="cursor-pointer rounded-md hover:opacity-80 transition-opacity"
+                />
+              </div>
+            </Card>
+            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="My Toolbox"
+                description="Explore the technologies and tools I use to craft exceptional
+                digital experiences."
+                className=""
+              />
+              <ToolboxItems
+                items={toolboxItems}
+                className=""
+                itemsWrapperClassName="animate-move-left [animation-duration:30s]"
+              />
+              <ToolboxItems
+                items={toolboxItems}
+                className="mt-6 "
+                itemsWrapperClassName="-translate-x-1/2 animate-move-right [animation-duration:15s]"
+              />
+            </Card>
+          </div>
+          <div className="grid gap-8 grid-cols-1 md:grid-cols-5 lg:grid-cols-3">
+            <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="Beyond the Code"
+                description="Explore my interests and hobbies beyond the digital realm."
+                className="px-6 py-6"
+              />
+              <div className="relative flex-1" ref={constraintRef}>
+                {hobbies.map((hobby) => (
+                  <motion.div
+                    key={hobby.title}
+                    className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute cursor-pointer"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                    drag
+                    dragConstraints={constraintRef}
+                  >
+                    <span className="font-medium text-gray-950">
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </motion.div>
+                ))}
+              </div>
+            </Card>
+            <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+              <Image
+                src={mapImage}
+                alt="Map Image"
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/20 ">
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
+                <Image
+                  src={smileMemoji}
+                  alt="Smile memoji"
+                  className="size-20"
+                />
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
